0591b0d6c662d28ff13a81356bae71ab39e8ccd5
MyPC <MyPC@MyPC-PC>
2019-09-06 10:38:02 +0530

    Second changes in class

--- Testproject/src/Testclass.java
+++ Testproject/src/Testclass.java
@@ -1,4 +1,18 @@
 
 public class Testclass {
+	
+	int a= 10;
+	int b = 20;
+	int c = a+b;
 
+
+public static void main(String[] args) {
+	int a= 10;
+	int b = 20;
+	int c = a+b;
+
+	System.out.println("add of a and b is:"+c);
+	
+
+}
 }
